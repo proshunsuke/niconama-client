f75788d8bda58c10daeb8d74d518814fdb72f722
proshunsuke <shunsuke0901@gmail.com>
2016-11-12 23:20:00 +0900

    add test

---
+++ test/niconamaClient.spec.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-expressions */
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import NiconamaClient from '../src/niconamaClient';
+
+describe('actions', () => {
+  it('run test', () => {
+    expect(true).to.equal(true);
+  });
+});
